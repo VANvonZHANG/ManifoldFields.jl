59b69dbad7d9d2bbd0de7ca3f2227b222164f029
Fan Zhang <vanvonzhang@gmail.com>
2026-07-22 18:24:43 +0800

    feat: add UGRID topology writer

--- src/ugrid.jl
+++ src/ugrid.jl
@@ -1,12 +1,70 @@
 import NCDatasets
+import ManifoldMeshes: cell_nodes, node_coordinates, num_cells, num_nodes
+
+struct UGridVariable{T}
+    data::T
+    dims::Tuple{Vararg{String}}
+    attrs::Dict{String,Any}
+end
 
 struct UGridDataset
-    variables::Dict{String,Any}
+    variables::Dict{String,UGridVariable}
     attributes::Dict{String,Any}
 end
 
-function to_ugrid(args...; kwargs...)
-    throw(ErrorException("to_ugrid is implemented in a later task"))
+function _lonlat(p)
+    r = sqrt(sum(abs2, p))
+    lat = asind(p[3] / r)
+    lon = mod(rad2deg(atan(p[2], p[1])), 360)
+    return lon, lat
+end
+
+function to_ugrid(mesh)
+    variables = Dict{String,UGridVariable}()
+
+    variables["Mesh2"] = UGridVariable(
+        0,
+        (),
+        Dict{String,Any}(
+            "cf_role" => "mesh_topology",
+            "topology_dimension" => 2,
+            "node_coordinates" => "Mesh2_node_lon Mesh2_node_lat",
+            "face_node_connectivity" => "Mesh2_face_nodes",
+            "face_dimension" => "n_face",
+        ),
+    )
+
+    node_lon = Vector{Float64}(undef, num_nodes(mesh))
+    node_lat = Vector{Float64}(undef, num_nodes(mesh))
+    for n in 1:num_nodes(mesh)
+        node_lon[n], node_lat[n] = _lonlat(node_coordinates(mesh, n))
+    end
+
+    variables["Mesh2_node_lon"] = UGridVariable(
+        node_lon,
+        ("n_node",),
+        Dict{String,Any}("standard_name" => "longitude", "units" => "degrees_east"),
+    )
+    variables["Mesh2_node_lat"] = UGridVariable(
+        node_lat,
+        ("n_node",),
+        Dict{String,Any}("standard_name" => "latitude", "units" => "degrees_north"),
+    )
+
+    face_nodes = Matrix{Int}(undef, num_cells(mesh), 4)
+    for c in 1:num_cells(mesh)
+        face_nodes[c, :] .= cell_nodes(mesh, c)
+    end
+    variables["Mesh2_face_nodes"] = UGridVariable(
+        face_nodes,
+        ("n_face", "n_max_face_nodes"),
+        Dict{String,Any}("cf_role" => "face_node_connectivity", "start_index" => 1),
+    )
+
+    return UGridDataset(
+        variables,
+        Dict{String,Any}("Conventions" => "CF-1.11 UGRID-1.0"),
+    )
 end
 
 function from_ugrid(args...; kwargs...)
---
+++ test/test_ugrid.jl
@@ -0,0 +1,37 @@
+using ManifoldFields
+using ManifoldMeshes
+using Test
+
+@testset "UGRID topology writer" begin
+    g = small_grid()
+    ds = to_ugrid(g)
+
+    @test ds.attributes["Conventions"] == "CF-1.11 UGRID-1.0"
+
+    @test haskey(ds.variables, "Mesh2")
+    meshvar = ds.variables["Mesh2"]
+    @test meshvar.attrs["cf_role"] == "mesh_topology"
+    @test meshvar.attrs["topology_dimension"] == 2
+    @test meshvar.attrs["node_coordinates"] == "Mesh2_node_lon Mesh2_node_lat"
+    @test meshvar.attrs["face_node_connectivity"] == "Mesh2_face_nodes"
+    @test meshvar.attrs["face_dimension"] == "n_face"
+
+    node_lon = ds.variables["Mesh2_node_lon"]
+    @test node_lon.dims == ("n_node",)
+    @test size(node_lon.data) == (num_nodes(g),)
+    @test node_lon.attrs["standard_name"] == "longitude"
+    @test node_lon.attrs["units"] == "degrees_east"
+
+    node_lat = ds.variables["Mesh2_node_lat"]
+    @test node_lat.dims == ("n_node",)
+    @test size(node_lat.data) == (num_nodes(g),)
+    @test node_lat.attrs["standard_name"] == "latitude"
+    @test node_lat.attrs["units"] == "degrees_north"
+
+    face_nodes = ds.variables["Mesh2_face_nodes"]
+    @test face_nodes.dims == ("n_face", "n_max_face_nodes")
+    @test size(face_nodes.data) == (num_cells(g), 4)
+    @test face_nodes.attrs["cf_role"] == "face_node_connectivity"
+    @test face_nodes.attrs["start_index"] == 1
+    @test Tuple(face_nodes.data[1, :]) == cell_nodes(g, 1)
+end
