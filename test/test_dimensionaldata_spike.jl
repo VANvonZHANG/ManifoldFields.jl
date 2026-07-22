aaf9f0d86d3ad0cbc517e6577266b104a7244f81
Fan Zhang <vanvonzhang@gmail.com>
2026-07-22 23:29:31 +0800

    fix: support selectors and unnamed field IO

--- test/test_dimensionaldata_spike.jl
+++ test/test_dimensionaldata_spike.jl
@@ -17,3 +17,29 @@ using Test
     @test sum(f) == sum(node_time_values(g))
     @test extrema(f) == extrema(node_time_values(g))
 end
+
+@testset "DiscreteField DimensionalData selector indexing" begin
+    g = small_grid()
+
+    f = DiscreteField(NodeLoc, g, node_time_values(g), node_time_dims(g);
+        name = :temperature)
+    time_slice = f[time = At(2)]
+    @test time_slice isa DiscreteField{NodeLoc}
+    @test mesh(time_slice) === g
+    @test data(time_slice) == node_time_values(g)[:, 2]
+    @test dims(time_slice) == node_dims(g)
+
+    dim_selector_slice = f[Dim{:time}(At(2))]
+    @test dim_selector_slice isa DiscreteField{NodeLoc}
+    @test mesh(dim_selector_slice) === g
+    @test data(dim_selector_slice) == node_time_values(g)[:, 2]
+    @test dims(dim_selector_slice) == node_dims(g)
+
+    ftn = DiscreteField(NodeLoc, g, time_node_values(g), time_node_dims(g);
+        name = :temperature)
+    time_first_slice = ftn[time = At(2)]
+    @test time_first_slice isa DiscreteField{NodeLoc}
+    @test mesh(time_first_slice) === g
+    @test data(time_first_slice) == time_node_values(g)[2, :]
+    @test dims(time_first_slice) == node_dims(g)
+end
