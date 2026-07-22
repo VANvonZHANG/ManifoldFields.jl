aacbce6adce3b80cca17af456a8b0fcf131e40a6
Fan Zhang <vanvonzhang@gmail.com>
2026-07-22 14:40:31 +0800

    chore: scaffold ManifoldFields package

---
+++ src/ManifoldFields.jl
@@ -0,0 +1,20 @@
+module ManifoldFields
+
+using DimensionalData
+using ManifoldMeshes
+using StaticArrays
+using YAXArrays
+
+export DiscreteField, mesh, data, location, withmesh
+export location_dimname, expected_location_length
+export interpolate
+export UGridDataset, to_ugrid, from_ugrid, from_ugrid_mesh, save_ugrid, load_ugrid,
+       load_ugrid_mesh
+
+include("locations.jl")
+include("field.jl")
+include("broadcast.jl")
+include("interpolate.jl")
+include("ugrid.jl")
+
+end
