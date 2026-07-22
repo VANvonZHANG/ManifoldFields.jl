4d7f684658c58fd5e58981f882d569c7f36f22a2
Fan Zhang <vanvonzhang@gmail.com>
2026-07-22 17:55:19 +0800

    fix: validate field broadcast axes

--- test/test_broadcast.jl
+++ test/test_broadcast.jl
@@ -6,6 +6,7 @@ using Test
 @testset "DiscreteField broadcast arithmetic" begin
     g = small_grid()
     f = DiscreteField(NodeLoc, g, node_values(g), node_dims(g); name=:a,
+                      refdims=(Dim{:time}(1:1),),
                       metadata=Dict("units" => "K"))
     h = DiscreteField(NodeLoc, g, 2 .* node_values(g), node_dims(g); name=:b,
                       metadata=Dict("units" => "K"))
@@ -16,6 +17,7 @@ using Test
     @test location(r) === NodeLoc
     @test data(r) == 3 .* node_values(g)
     @test dims(r) == node_dims(g)
+    @test DimensionalData.refdims(r) == DimensionalData.refdims(f)
     @test DimensionalData.name(r) == :a
     @test DimensionalData.metadata(r)["units"] == "K"
 
@@ -23,21 +25,33 @@ using Test
     @test s isa DiscreteField{NodeLoc}
     @test mesh(s) === g
     @test data(s) == sin.(node_values(g))
+    @test DimensionalData.refdims(s) == DimensionalData.refdims(f)
 
     t = 2 .* f
     @test t isa DiscreteField{NodeLoc}
     @test data(t) == 2 .* node_values(g)
     @test DimensionalData.name(t) == :a
+    @test DimensionalData.refdims(t) == DimensionalData.refdims(f)
 
     arr_result = f .+ ones(num_nodes(g))
     @test arr_result isa DiscreteField{NodeLoc}
     @test data(arr_result) == node_values(g) .+ 1
+    @test DimensionalData.refdims(arr_result) == DimensionalData.refdims(f)
 
     dim_array = DimArray(ones(num_nodes(g)), node_dims(g))
     dim_result = f .+ dim_array
     @test dim_result isa DiscreteField{NodeLoc}
     @test mesh(dim_result) === g
     @test data(dim_result) == node_values(g) .+ 1
+    @test DimensionalData.refdims(dim_result) == DimensionalData.refdims(f)
+
+    wrong_name = DimArray(ones(num_nodes(g)), (Dim{:foo}(1:num_nodes(g)),))
+    @test_throws DimensionMismatch f .+ wrong_name
+
+    shifted_lookup = DimArray(ones(num_nodes(g)), (Dim{:node}(2:(num_nodes(g) + 1)),))
+    @test_throws DimensionMismatch f .+ shifted_lookup
+
+    @test_throws DimensionMismatch f .+ ones(num_nodes(g), 1)
 
     g2 = small_grid()
     same_shape_other_mesh = DiscreteField(NodeLoc, g2, node_values(g2), node_dims(g2))
